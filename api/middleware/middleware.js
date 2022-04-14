f418e84c4baddb67f7c13608399e1f8fb49213ed
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-13 18:59:38 -0600

    set up logger middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
+// Users = require('./api/users')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('logger middleware')
+  console.log(`[${new Date().toLocaleString()}] ${req.method} to ${req.url} `)
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUserId')
+  next()
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUser')
+  next()
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validatePost')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+server.listen(9000, () => {
+  console.log('Server working on port 9000')
+})
